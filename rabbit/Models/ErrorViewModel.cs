f51cf34278a00770b441914aca824d373da54f4c
richie <richie.kwon1@gmail.com>
2022-06-15 20:28:26 +0100

    Initial commit

---
+++ rabbit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rabbit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
