b16f5ff8c23d093314da5af7def79dc063bc75ab
richie <richie.kwon1@gmail.com>
2022-06-25 13:08:54 +0100

    access denied

--- AspNote/DataContext/AspNoteDbContext.cs
+++ AspNote/DataContext/AspNoteDbContext.cs
@@ -2,6 +2,7 @@ using System.Configuration;
 using System.Data.Common;
 using AspNote.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
@@ -9,20 +10,33 @@ namespace AspNote.DataContext
 {
     public class AspNoteDbContext : DbContext
     {
-        public AspNoteDbContext(DbContextOptions<AspNoteDbContext> options): base(options)
-        {
-            
-        }
-      
+        // public AspNoteDbContext(DbContextOptions<AspNoteDbContext> options): base(options)
+        // {
+        //     
+        // }
+        //
 
+        protected readonly IConfiguration Configuration;
         public AspNoteDbContext()
         {
            
-        }  
+        }
+
+        public AspNoteDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+      
 
         public DbSet<User> Users { get; set; } 
         public DbSet<Note> Notes { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connnectionString = Configuration.GetConnectionString("Default");
+            options.UseMySql(connnectionString, ServerVersion.AutoDetect(connnectionString));
+        }
         // protected override void OnModelCreating(DbModelBuilder  modelBuilder)
         // {
         //     base.OnModelCreating(modelBuilder);
@@ -33,19 +47,25 @@ namespace AspNote.DataContext
         // DB connection
         // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         // {
-        //     optionsBuilder.UseMySQL(@"Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;",
-        //         options => optioinsBuilder.Enable);
+        //     if (!optionsBuilder.IsConfigured)
+        //     {
+        //
+        //         optionsBuilder.UseMySQL("@Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;");
+        //     }
+        //
+        //     // optionsBuilder.UseMySQL(@"Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;",
+        //     //     options => optioinsBuilder.Enable);
         // }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql(@"Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;");
-
-            // optionsBuilder.UseMySql(
-            //     ServerVersion.AutoDetect(
-            //         "Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;"),
-            //         options => options.EnableRetryOnFailure( ));
-        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder ptionsBuilder)
+        // {
+        //     // optionsBuilder.UseMySql(ServerVersion.AutoDetect("Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;"));
+        //
+        //     optionsBuilder.UseMySql(
+        //         ServerVersion.AutoDetect(
+        //             "Server=127.0.0.0;Database=myNoteDb;Uid=root;Pwd=dookie91Sql!;"),
+        //             options => options.EnableRetryOnFailure( ));
+        // }
         
     } 
 } 
