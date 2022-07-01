4f308f7fc04f70ca2dd8beb447c331e25dc2c7c2
richie <richie.kwon1@gmail.com>
2022-07-01 21:35:42 +0100

    detail pages

--- AspNote/Controllers/NoteController.cs
+++ AspNote/Controllers/NoteController.cs
@@ -27,6 +27,21 @@ namespace AspNote.Controllers
             
         }
 
+        public IActionResult Detail(int noteNo)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY")==null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            using (var db = new AspNoteDbContext())
+            {
+                var note = db.Notes.FirstOrDefault(n => n.NoteNo.Equals(noteNo));
+                return View(note);
+            }
+            
+        }
+
         public IActionResult Add()
         {
             if (HttpContext.Session.GetInt32("USER_LOGIN_KEY")==null)
